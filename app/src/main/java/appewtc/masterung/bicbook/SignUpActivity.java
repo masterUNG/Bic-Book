a84feb62de891bdefd542c85e3318bc8a249a33f
masterUNG <phrombutr@gmail.com>
2016-03-09 11:10:32 +0700

    Check Space Finish

--- app/src/main/java/appewtc/masterung/bicbook/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/bicbook/SignUpActivity.java
@@ -1,13 +1,53 @@
 package appewtc.masterung.bicbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText idCardEditText, passwordEditText;
+    private String idCardString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+        idCardEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
     }
-}
+
+    public void clickSignUpSign(View view) {
+
+        idCardString = idCardEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (idCardString.equals("") || passwordString.equals("")) {
+            //Have Space
+            myToast("กรุณากรอกให้ครบ คะ");
+
+        } else {
+            //No Space
+
+        }
+
+
+    }   // clickSignUp
+
+    private void myToast(String strToase) {
+        Toast.makeText(SignUpActivity.this, strToase, Toast.LENGTH_SHORT).show();
+    }
+
+}   // Main Class
