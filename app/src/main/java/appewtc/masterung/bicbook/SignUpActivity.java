0147e7d8740e45d1baff79f45fcb7afb3ccdd0a2
masterUNG <phrombutr@gmail.com>
2016-03-16 11:20:13 +0700

    Post Data to mySQL finish

--- app/src/main/java/appewtc/masterung/bicbook/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/bicbook/SignUpActivity.java
@@ -3,10 +3,20 @@ package appewtc.masterung.bicbook;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -89,6 +99,32 @@ public class SignUpActivity extends AppCompatActivity {
     }   // confirmData
 
     private void updateDataToServer() {
+
+        //Connected Http
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            nameValuePairs.add(new BasicNameValuePair("ID_Card", idCardString));
+            nameValuePairs.add(new BasicNameValuePair("Password", passwordString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/bic/php_add_user_master.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            httpClient.execute(httpPost);
+            myToast("อัพเดทข้อมูล บน Server แว้วววว");
+            finish();
+
+        } catch (Exception e) {
+            myToast("ไม่สามารถเชื่อมต่อ Server ได้");
+        }
+
+
+
 
     }   // updateDataToServer
 
